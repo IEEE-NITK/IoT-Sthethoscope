31289f20781f89ea02cea8ad5cae6a4fdce7d84c
jeetrshah <jeetshah250@gmail.com>
2020-10-28 02:45:52 +0530

    Status updates for review

--- sourceCode/Filter/BPF.py
+++ sourceCode/Filter/BPF.py
@@ -5,7 +5,7 @@ import numpy as np
 import soundfile as sf
 #Read the Wav file and plotting it
 data,sampleRate = sf.read('102_1b1_Ar_sc_Meditron.wav')
-fo, to, Sxxo = signal.spectrogram(data,fs=sampleRate,nfft=512,nperseg=512)
+fo, to, Sxxo = signal.spectrogram(data)
 fig = plt.figure()
 ax = fig.add_subplot(3, 1, 1)
 plt.pcolormesh(to, fo, Sxxo, shading='gouraud')
@@ -15,7 +15,7 @@ ax.set_ylabel('Frequency')
 ax.grid(which='both', axis='both')
 
 #Design the Filter and Plot it.
-sos = signal.butter(100, [100,5000], btype='bandpass', analog=False, output='sos', fs=sampleRate)
+sos = signal.butter(20, [100,5000], btype='bandpass', analog=False, output='sos', fs=sampleRate)
 w, h = signal.sosfreqz(sos, 44100, fs=44100)
 ax = fig.add_subplot(3, 1, 2)
 ax.semilogx(w, 20 * np.log10(np.maximum(abs(h), 1e-5)))
@@ -27,7 +27,7 @@ ax.grid(which='both', axis='both')
 
 #Filtering the signal and plotting the filtered Signal
 y = signal.sosfilt(sos,data)
-ff, tf, Sxxf = signal.spectrogram(y)
+ff, tf, Sxxf = signal.spectrogram(y,return_onesided=False)
 ax = fig.add_subplot(3, 1, 3)
 plt.pcolormesh(tf, ff, Sxxf, shading='gouraud')
 ax.set_title('Filtered Signal')
