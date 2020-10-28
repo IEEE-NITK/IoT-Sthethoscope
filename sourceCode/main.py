3d0eaa39f3888cdd7c1594390cc819f2de3d87df
jeetrshah <jeetshah250@gmail.com>
2020-10-28 20:59:48 +0530

    main File changed

--- sourceCode/main.py
+++ sourceCode/main.py
@@ -1,1 +1,68 @@
-print('Hello World')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from scipy import signal
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import soundfile as sf
+import keras
+from keras.models import Sequential
+from keras.datasets import mnist
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.initializers import Constant
+from keras import backend as K
+from keras.layers import PReLU
+
+inputData = []
+targetData = []
+root = '../input/respiratory-sound-database/Respiratory_Sound_Database/Respiratory_Sound_Database/audio_and_txt_files/'
+filenames = [s.split('.')[0] for s in os.listdir(path = root) if '.txt' in s]
+i_list = []
+rec_annotations = []
+rec_annotations_dict = {}
+
+def Extract_Annotation_Data(file_name, root):
+    tokens = file_name.split('_')
+    recording_info = pd.DataFrame(data = [tokens], columns = ['Patient number', 'Recording index', 'Chest location','Acquisition mode','Recording equipment'])
+    recording_annotations = pd.read_csv(os.path.join(root, file_name + '.txt'), names = ['Start', 'End', 'Crackles', 'Wheezes'], delimiter= '\t')
+    return (recording_info, recording_annotations)
+
+def slice_data(start, end, raw_data,  sample_rate):
+    max_ind = len(raw_data)
+    start_ind = min(int(start * sample_rate), max_ind)
+    end_ind = min(int(end * sample_rate), max_ind)
+    return raw_data[start_ind: end_ind]
+
+def getClass(df,index):
+    if(df.at[index,'Wheezes']==0 and df.at[index,'Crackles']==0):
+        return 0
+    elif(df.at[index,'Wheezes']==1 and df.at[index,'Crackles']==0):
+        return 1
+    elif(df.at[index,'Wheezes']==0 and df.at[index,'Crackles']==1):
+        return 2
+    elif(df.at[index,'Wheezes']==1 and df.at[index,'Crackles']==1):
+        return 3
+
+for s in filenames:
+    (i,a) = Extract_Annotation_Data(s, root)
+    i_list.append(i)
+    rec_annotations.append(a)
+    rec_annotations_dict[s] = a
+recording_info = pd.concat(i_list, axis = 0)
+recording_info.head()
+
+l=0
+for i in rec_annotations_dict:
+    j = rec_annotations_dict[i]
+    print(l)
+    l=l+1
+    for k in range(j.shape[0]):
+        data,sampleRate = sf.read(root+i+'.wav')
+        inputData = np.append(inputData,slice_data(j.at[k,'Start'],j.at[k,'End'], data, sampleRate),axis=0)
+        targetData = np.append(targetData,getClass(j,k))
+
+print(inputData)
+print(targetData)
+print(target)
